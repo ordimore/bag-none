5a124d750d381dcac3a879ee8d769df6b5369b25
wilmar <wilmar.nugroho@gmail.com>
2023-07-07 19:48:35 +0700

    product selesai
    penambahan product dan show less

--- script/script.js
+++ script/script.js
@@ -1,9 +1,87 @@
-let buttonmore = document.getElementById("show-more");
-let cssrow3 = document.getElementById("product-row3");
-let containerproduct = document.getElementById("container-product");
-//buttonmore.addEventListener("click",row3visible());
-function row3visible(){
-    containerproduct.style.height = "999px";
-    cssrow3.style.visibility = "visible";
-    buttonmore.style.visibility = "hidden";
+let btn_show = document.getElementById("show-item");
+let row3 = document.getElementById("product-row3");
+
+function show_more_item(){
+    row3.innerHTML = 
+        `
+        <div class="product1">
+            <div class="product-img">
+                <img src="./img/product/9.webp" alt="">
+            </div>
+            <div class="product-name">
+                <p>Dickies x Pop Trading Company Tote Bag</p>
+            </div>
+            <div class="product-harga">
+                <p>IDR 440.000,00</p>
+            </div>
+            <div class="product-buy-menu">
+                <button>-</button><p>1</p><button>+</button>
+            </div>
+            <div class="product-buy-button">
+            <p>BUY NOW</p>
+            </div>
+        </div>
+        <div class="product2">
+            <div class="product-img">
+                <img src="./img/product/10.webp" alt="">
+            </div>
+            <div class="product-name">
+                <p>Parra Hot Springs Pattern Beach Bag</p>
+            </div>
+            <div class="product-harga">
+                <p>IDR 435.000,00</p>
+            </div>
+            <div class="product-buy-menu">
+                <button>-</button><p>1</p><button>+</button>
+            </div>
+            <div class="product-buy-button">
+            <p>BUY NOW</p>
+            </div>
+        </div>
+        <div class="product1">
+            <div class="product-img">
+                <img src="./img/product/11.webp" alt="">
+            </div>
+            <div class="product-name">
+                <p>Carhartt Philis Backpack</p>
+            </div>
+            <div class="product-harga">
+                <p>IDR 856.000,00</p>
+            </div>
+            <div class="product-buy-menu">
+                <button>-</button><p>1</p><button>+</button>
+            </div>
+            <div class="product-buy-button">
+            <p>BUY NOW</p>
+            </div>
+        </div>
+        <div class="product2">
+            <div class="product-img">
+                <img src="./img/product/12.webp" alt="">
+            </div>
+            <div class="product-name">
+                <p>Patagonia Altvia Pack 28L</p>
+            </div>
+            <div class="product-harga">
+                <p>IDR 964.000,00</p>
+            </div>
+            <div class="product-buy-menu">
+                <button>-</button><p>1</p><button>+</button>
+            </div>
+            <div class="product-buy-button">
+            <p>BUY NOW</p>
+            </div>
+        </div>
+    `;
+    btn_show.innerHTML = "SHOW LESS";
+    btn_show.setAttribute('onclick','show_less_item()');
 }
+
+function show_less_item(){
+    row3.innerHTML = 
+        `<div>
+    </div>`;
+    btn_show.innerHTML = "SHOW MORE";
+    btn_show.setAttribute('onclick','show_more_item()');
+}
+
